1c83e59d02db4362da5c3b34a563cf0ff73cf3d6
farhangalib-1 <farhan.galib_cumilla1@yahoo.com>
2026-02-18 19:00:32 +0600

    cashout logic is implemented

--- scripts/cashout.js
+++ scripts/cashout.js
@@ -3,13 +3,32 @@ document.getElementById('cashoutbtn').addEventListener('click', function(){
     const amount = document.getElementById('cashoutamount')
     const pin = document.getElementById('cashoutpin')
     const balance = document.getElementById('balance')
-    
     if(parseInt(amount.value) <= parseInt(balance.innerText)){
-        console.log('ammount is available');
-    }
-    else{
-        console.log('amount is not available');
+        if(accountNumber.value.length === 11 && pin.value === '1234'){
+        const newBalance = parseInt(balance.innerText) - parseInt(amount.value)
+        balance.textContent = newBalance
+        alert(`৳${amount.value} is widthdraw successfully ${new Date().toDateString()}`)
+        }
+        else{
+            alert('Mobile Number or Pin number is invalid')
+            accountNumber.value = ''
+            amount.value = ''
+            pin.value = ''
+            return
+        }
         
+    }
+    if(amount.value === ''){
+            alert('Please enter your amount')
+             accountNumber.value = ''
+            amount.value = ''
+            pin.value = ''
+            return
+        }
+    if(amount.value < 0 || parseInt(amount.value) > parseInt(balance.innerText)){
+        alert('Insufficent Balance. Please Try again')
+        pin.value = ''    
+        return
     }
     
 })
